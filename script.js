524462d825e126f16f55258218d493bfef856b35
Cubinx09 <lalaneamanda0923@gmail.com>
2026-04-08 21:26:47 -0400

    Step 3 of 5 done. I'm just getting started if you think about it.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// WORKING CLOCK //
-
-
-// WORKING CLOCK//
-
+// CLOCK LOGIC
 function updateTime() {
 		// converts data of date and time into a string //
         var currentTime = new Date().toLocaleString();
@@ -16,3 +12,77 @@ function updateTime() {
 
     // function runds every 1 second//
     setInterval(updateTime, 1000);
+
+
+// MAKE THE DIV ELEMENT DRAGGABLE
+dragElement(document.getElementById("welcome"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// RESIZE DIV ELEMENTS
+
+
+// 'OPEN' AND 'CLOSE' DIV ELEMENTS
+var welcomeScreen = document.querySelector("#welcome");
+var welcomeScreenClose = document.querySelector("#welcomeClose");
+var welcomeScreenOpen = document.querySelector("#welcomeOpen");
+
+
+// makes window dissapear when clicking on the close button
+function closeWindow(elmnt){
+  elmnt.style.display = "none";
+}
+
+// makes welcome window reappear when clicking on 'CubeOS'
+function openWindow(elmnt){
+  elmnt.style.display = "block";
+}
+
+// makes the close function an event
+welcomeScreenClose.addEventListener("click", function(){
+  closeWindow(welcomeScreen);
+});
+
+// makes the open function an event
+welcomeScreenOpen.addEventListener("click", function(){
+  openWindow(welcomeScreen);
+});

0fcd7b0db57d6d1e16016f09efa718ef6e06acc9
Cubinx09 <lalaneamanda0923@gmail.com>
2026-04-22 19:27:17 -0400

    More images turned into webp. 3rd app complete.

--- script.js
+++ script.js
@@ -29,9 +29,9 @@ dragElement(document.querySelector("#art"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.querySelector(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.querySelector(elmnt.id + "-header").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
